d9188f08537ecf1057fa13a6e4a9b3c9f7c8fe9d
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-01 22:55:49 -0400

    needs update on readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Ten's Latest StopWatch 
+**Now Available, Only on Console Logs near You!**
+This mini project was a huge challenge for me. (tenzin, 4-1-23)
+instantiating Date and using getTime() method!
+
+**Link to project:** ##.com
+
+![alt tag](http://placecorgi.com/1200/650)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, 
+
+Starting with more focused and smaller projects allowed me to embrace versatile functions and all its capabilities. Instantiating Date and using getTime() method!
+
+## Optimizations
+*function code block repitition to get comfortable with javaScript*
+
+## Lessons Learned:
+
+Learned that everything commmented out will not affect the speed & optimization on a site. With the codeblock re-written and commented out a dozen times, I was worried
+that it may affect the speed. This allowed me to get more comfortable with adding comments to each of my steps in future projects and work.  
+Learned that with practice can come perfect. One step closer to perfecting the foundations of user interaction. 
+
+Impletementing eventListeners and functions. 
+
+*fuck yeah I did it!*
+
+also, CSS gets easier when JS takes half the load. 
---
+++ main.js
@@ -0,0 +1,114 @@
+function StopWatch(){
+    let startTime, endTime, running, duration = 0
+
+    this.start = function(){
+        if(running){
+            throw new Error('chill shorty you already started the stopwatch')
+        }
+        running= true;
+        startTime= new Date()
+    }
+    this.stop = function(){
+        if(!running){
+            throw new Error('start it before you stop it, like damn.')
+        }
+        running= false
+        startTime= new Date();
+        let seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds
+    }
+
+    this.reset = function(){
+       startTime= null;
+       endTime= null;
+       running= false;
+       duration= 0;
+    }
+    Object.defineProperty(this, 'duration', {
+        get: function(){
+            return duration
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Stopwatch(){
+//     let startTime, endTime, running, duration = 0;
+
+//     this.start = function(){
+//         if(running)
+//             throw new Error('Stopwatch has already started.');
+        
+//         running = true;
+        
+//         startTime = new Date();
+
+//     }
+
+//     this.stop = function(){
+//         if (!running)
+//             throw new Error('Stopwatch is not started.');
+        
+//         running = false;
+
+//         endTime = new Date();
+
+//         const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+//         duration += seconds;
+//     }
+
+//     this.reset = function(){
+//         startTime = null;
+//         endTime = null;
+//         running = false;
+//         duration = 0; 
+//     }
+
+//     Object.defineProperty(this, 'duration', {
+//         get: function() { return duration; }
+//     })    
+
+    
+
+// }
+
