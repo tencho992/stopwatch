bc2187f00dcd614b93a191d6d01617daceb0b846
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-02 16:06:43 -0400

    throw a new Error widdit

--- main.js
+++ main.js
@@ -1,32 +1,57 @@
+//creating a function for StopWatch 
+//in console.log-> create var for stopwatch 
+//and instantiate new StopWatch()
 function StopWatch(){
-    let startTime, endTime, running, duration = 0
 
+    //declare initial value for all variables in equation.
+    let startTime, endTime, running, display = 0
+
+    //create methods that reflect all variables.
     this.start = function(){
+        //start= the start of running
+        //create a console.log error when multiple starts are inputted
         if(running){
-            throw new Error('chill shorty you already started the stopwatch')
+            //have fun with the message, you're developing this program.
+            //throw new Error('')
+            throw new Error('Chill shorty hasbeen you already started the stopwatch')
         }
         running= true;
+        //running will switch to false when sw has been stopped
+
+        //reassign startTime var to date method by instantiating (?)object(?)
         startTime= new Date()
+        //is Date() prototype of a String? Wow,..
     }
     this.stop = function(){
+        //stop === not runnning 
         if(!running){
-            throw new Error('start it before you stop it, like damn she in ha mood.')
+            //Throw Them a New Error//**Throw them a new error */
+            //when you throw em a new Error, get jiggy widdit
+            //Throw a new Error widdit **
+            throw new Error('start it before you stop it, like damn, she in ha mood.')
         }
         running= false
-        startTime= new Date();
+        //instantiating end date 
+        endTime= new Date();
+        //endTime now has the inherited .getTime() method
+        //.getTime() will return milliseconds, divide product by 1,000
+        //create var to store info
         let seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-        duration += seconds
+        //declare display that adds stored value to display
+        display += seconds
     }
-
+    //create a new method for reset
     this.reset = function(){
        startTime= null;
        endTime= null;
        running= false;
-       duration= 0;
+       display= 0;
     }
-    Object.defineProperty(this, 'duration', {
+    Object.defineProperty(this, 'display', {
+
+        //why is it get and not set?
         get: function(){
-            return duration
+            return display
         }
     })
 }
@@ -37,7 +62,7 @@ function StopWatch(){
 
 
 // function Stopwatch(){
-//     let startTime, endTime, running, duration = 0;
+//     let startTime, endTime, running, display = 0;
 
 //     this.start = function(){
 //         if(running)
@@ -58,18 +83,18 @@ function StopWatch(){
 //         endTime = new Date();
 
 //         const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-//         duration += seconds;
+//         display += seconds;
 //     }
 
 //     this.reset = function(){
 //         startTime = null;
 //         endTime = null;
 //         running = false;
-//         duration = 0; 
+//         display = 0; 
 //     }
 
-//     Object.defineProperty(this, 'duration', {
-//         get: function() { return duration; }
+//     Object.defineProperty(this, 'display', {
+//         get: function() { return display; }
 //     })    
 
     
